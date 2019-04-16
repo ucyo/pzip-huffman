a0396d9271bffab459d107208913cc17f6628924
ucyo <cayoglu@me.com>
2019-04-16 10:09:56 +0200

    Add example

---
+++ examples/simple.rs
@@ -0,0 +1,22 @@
+use pzip_huffman;
+
+fn main() {
+
+    let words = "mmmaaaassssssssssssii";
+    let codes = pzip_huffman::get_huffman_codes(&words);
+    println!("Codetable: {:#?}", codes);
+
+    let word = "mass";
+    let coded = pzip_huffman::encode(&word, &codes);
+    println!("huff('mass'): {:?}", coded);
+
+    let coded_bytes = coded.to_bytes();
+    println!("huff('mass'): {:?} [bytes]", coded_bytes);
+
+    print!("huff('mass'): ");
+    for val in coded_bytes {
+        print!("{:08b} ", val);
+    }
+    println!("");
+
+}
